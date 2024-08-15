84f4a1d902c6f501ba7ed0f4465e5ac1853bdbe5
John2309j <johnjebaraj361@gmail.com>
2024-08-16 01:27:55 +0530

    initial-commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import MovieHome from './Components/MovieHome';
+
+function App() {
+  return (
+    <div className="App">
+     <MovieHome/>
+    </div>
+  );
+}
+
+export default App;
